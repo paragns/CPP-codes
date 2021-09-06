1c91363efd2ed82de56460a7aea85a38bd83afce
paragns <paragns1010@gmail.com>
2021-09-06 17:31:17 +0530

    Create aaaa (1).cpp

---
+++ aaaa (1).cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+# define ll long long
+#define mod 1000000007
+#define inf INT_MAX
+#define minf INT_MIN
+int main(){
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+	ll int T=1;
+	//cin>>T;
+	 while(T--){
+	 	int n; cin>>n;
+	 	vector<int>A;
+	}
+return 0;
+}
