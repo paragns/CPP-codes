4c2a304e89783b418185fab6aa4e40ac66ed6a27
paragns <paragns1010@gmail.com>
2021-11-17 17:02:32 +0530

    new

--- lcp adloid.cpp
+++ lcp adloid.cpp
@@ -3,26 +3,30 @@ using namespace std;
 int main(){
 	int n;
 	cin>>n;
-	vector<vector<int>>lcp;
+	vector<vector<int>>lcp(n,vector<int>(n,0));
 	for(int i=0;i<n;i++){
-		for(int j=0;j<n;j++)cin>>A[i][j];
+		for(int j=0;j<n;j++){
+			cin>>lcp[i][j];
+			if(i+lcp[i][j]>n or j+lcp[i][j]<n){
+				cout<<"Impossible";
+				return 0;	
+			}
+		}
+		
 	}
-	char curr='a';
 	string ans="";
 	for(int i=0;i<n;i++)ans+='@';
-	for(int j=1;j<n;j++){
-		for(int i=0;i<n;i++){
-				int k=i+j;
-				int x=lcp[i][k];
-				if(x){
-					char c=curr;
-					if(ans[i]!='@')c=ans[i];
-					while(x--){
-						ans+=c;
-						c= c+1;
-					}
+	ans[0]='a';
+	for(int i=0;i<n;i++){
+		for(int j=0;j<n;j++){
+				int x=lcp[i][j];
+				if(ans[i]=='@')ans[i]=(char)(ans[i-1]+1);
+				if(ans[i]>'z')ans="Impossible";
+				while(x--){
+					ans[j]=ans[i];
 				}
 		}
+		if(ans=="Impossible")break;
 	}
-
+	cout<< ans;
 }
